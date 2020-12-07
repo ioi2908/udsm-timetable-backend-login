cf2606661a0cc328293851c46438bec0058a15c4
Stanley <2001stany@gmail.com>
2020-12-07 19:56:21 +0300

    CORS error fixed

--- run.py
+++ run.py
@@ -2,8 +2,10 @@ from app import app
 from  flask import Flask
 
 
-
+app.config['CORS_HEADERS'] = 'Content-Type'
 app = Flask(__name__)
+
+CORS(app, resource={"origin":"https://timetableudsmstudent.herokuapp.com/"})
 
 if __name__ == "__main__":
 	app.run(debug=True)
